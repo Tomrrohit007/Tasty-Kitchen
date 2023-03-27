d46d91b598990d7fbdfc2a5d52847a8b5a713ebc
Tomrrohit007 <tomarrohit5034@gmail.com>
2023-03-27 12:31:13 +0530

    solved

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { ContextProvider } from "./components/CartContext/CartListContext";
 import { HeaderProvider } from "./components/CartContext/HeaderContext";
 
@@ -10,9 +10,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <HeaderProvider>
       <ContextProvider>
-        <BrowserRouter>
+        <HashRouter>
           <App />
-        </BrowserRouter>
+        </HashRouter>
       </ContextProvider>
     </HeaderProvider>
   </React.StrictMode>
